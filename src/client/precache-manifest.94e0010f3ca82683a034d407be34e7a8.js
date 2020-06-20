a5a06252ee0f4b9a3e00b6124f21ef9a86c271b0
Nick Maslov <kategorychnost@gmail.com>
2020-06-19 20:34:03 -0400

    added build versions from server and from client

---
+++ src/client/precache-manifest.94e0010f3ca82683a034d407be34e7a8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cfbbbcc3d75ba186b46e946987cdcb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23aad33892add1dc7c3",
+    "url": "/static/css/2.edb8eddf.chunk.css"
+  },
+  {
+    "revision": "b4eb0430212cecb96e9d",
+    "url": "/static/css/main.67d0c795.chunk.css"
+  },
+  {
+    "revision": "f23aad33892add1dc7c3",
+    "url": "/static/js/2.66452819.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.66452819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4eb0430212cecb96e9d",
+    "url": "/static/js/main.754710f8.chunk.js"
+  },
+  {
+    "revision": "b834c3e50948291620c3",
+    "url": "/static/js/runtime-main.a46f58ae.js"
+  },
+  {
+    "revision": "cd98e2c39b50532ef09a3344e820499f",
+    "url": "/static/media/cancun.cd98e2c3.jpg"
+  },
+  {
+    "revision": "df994899b2925871e24126812c416c24",
+    "url": "/static/media/dubai.df994899.jpg"
+  },
+  {
+    "revision": "841651c32f46a04c181eb090a32bd103",
+    "url": "/static/media/london.841651c3.jpg"
+  },
+  {
+    "revision": "73d8ec963252e42850a528642374f476",
+    "url": "/static/media/los-angeles.73d8ec96.jpg"
+  },
+  {
+    "revision": "c21df1475cabff80a179b3c0cd49dd67",
+    "url": "/static/media/map-background.c21df147.jpg"
+  },
+  {
+    "revision": "23aadf8146a0240ddc18f846d8c64843",
+    "url": "/static/media/san-fransisco.23aadf81.jpg"
+  },
+  {
+    "revision": "3c573d340e505c3f2f5b60c34b20aacc",
+    "url": "/static/media/toronto.3c573d34.jpg"
+  }
+]);
